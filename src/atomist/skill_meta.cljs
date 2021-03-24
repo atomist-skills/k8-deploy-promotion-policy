8a7739d132eaa8a2e7cf36978d32659878903f3d
Jim Clark <jim@atomist.com>
2021-03-23 23:08:19 -0700

    Bump k8-deploy-promotion-policy to 0.1.14 and spit metadata

--- src/atomist/skill_meta.cljs
+++ src/atomist/skill_meta.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "k8-deploy-promotion-policy")
 (def path ".")
-(def version "0.1.13")
-(def tag "0.1.13")
-(def generated-at "2021-03-23T23:06:20Z")
+(def version "0.1.14")
+(def tag "0.1.14")
+(def generated-at "2021-03-24T06:08:19Z")
